ae67800e7d7cf21d5fde08cc04bb5a0c5754ea5a
supunkamburugamuve <supun06@gmail.com>
2016-07-31 13:17:53 -0400

    eventloop additions

--- test/hps_client.cpp
+++ test/hps_client.cpp
@@ -22,11 +22,11 @@ int connect() {
 int connect3() {
   Client client(&options, hints);
   client.Connect();
-  client.Start();
   while (con == NULL) {
     sleep(1);
     con = client.GetConnection();
   }
+  client.Start();
   return 1;
 }
 
