94e708d38c3ba3bffd89f343e8ca9c3d7bd79aa2
supunkamburugamuva <supun06@gmail.com>
2016-08-08 11:04:59 -0400

    changing

--- src/connection.cpp
+++ src/connection.cpp
@@ -656,7 +656,7 @@ int Connection::TransmitComplete() {
   size_t max_completions = tx_seq - tx_cq_cntr;
   // we can expect up to this
   max_completions = max_completions == 0 ? 0 : max_completions;
-  HPS_INFO("Transmit complete max_completions=%ld tx_seq=%ld tx_cq_cntr=%ld", max_completions, tx_seq, tx_cq_cntr);
+  HPS_INFO("Transmit complete max_completions=%ld tx_seq=%ld tx_cq_cntr=%ld rx_seq=%ld rx_cq_cntr=%ld", max_completions, tx_seq, tx_cq_cntr, rx_seq, rx_cq_cntr);
   ssize_t cq_ret = fi_cq_read(txcq, &comp, max_completions);
   if (cq_ret > 0) {
     this->tx_cq_cntr += cq_ret;
@@ -693,7 +693,7 @@ int Connection::ReceiveComplete() {
   size_t max_completions = rx_seq - rx_cq_cntr;
   // we can expect up to this
   max_completions = max_completions == 0 ? 0 : max_completions;
-  HPS_INFO("Receive complete max_completions=%ld rx_seq=%ld rx_cq_cntr=%ld", max_completions, rx_seq, rx_cq_cntr);
+  HPS_INFO("Receive complete max_completions=%ld tx_cq_cntr=%ld rx_seq=%ld rx_cq_cntr=%ld", max_completions, tx_seq, tx_cq_cntr, rx_seq, rx_cq_cntr);
   ssize_t cq_ret = fi_cq_read(rxcq, &comp, max_completions);
   if (cq_ret > 0) {
     this->rx_cq_cntr += cq_ret;
