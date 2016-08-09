7949a9c36bb35292d2e0d66604f452a73cff0f08
supunkamburugamuva <supun06@gmail.com>
2016-08-09 11:22:09 -0400

    changing

--- test/hps_client.cpp
+++ test/hps_client.cpp
@@ -37,7 +37,7 @@ int exchange3() {
   HPS_INFO("Done sending.. switching to receive");
   while (read < 4000 && count < 10) {
     if (con->DataAvailableForRead()) {
-      con->ReadData((uint8_t *) (values[0] + read), sizeof(values[0]) - read, &current_read);
+      con->ReadData(((uint8_t *) (values[0]) + read), sizeof(values[0]) - read, &current_read);
       HPS_INFO("read amount %d", current_read);
       read += current_read;
       count++;
