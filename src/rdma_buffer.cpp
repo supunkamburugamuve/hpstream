3b1b93b0f09473955fa28be6f84f3264318b7e55
supunkamburugamuve <supun06@gmail.com>
2016-08-16 22:21:29 -0400

    eventloop additions

--- src/rdma_buffer.cpp
+++ src/rdma_buffer.cpp
@@ -95,6 +95,7 @@ int RDMABuffer::IncrementTail(uint32_t count) {
   this->submitted_buffs -= count;
   this->filled_buffs -= count;
   // signal that we have an empty buffer
+  HPS_INFO("Increment tail, wait no longer");
   pthread_cond_signal(&cond_empty);
   return 0;
 }
