bd8b2c322f4679ee2051a0b77ff8544dc5d8ca21
supunkamburugamuve <supun06@gmail.com>
2016-08-21 00:09:41 -0400

    eventloop additions

--- test/hps_client.cpp
+++ test/hps_client.cpp
@@ -64,7 +64,7 @@ int exchange3() {
   }
   clock_gettime(CLOCK_MONOTONIC, &end);
   rate = 1000000.0 * 4000.0 /((1024 * 1024)* (elapsed / (1000 * 1000)));
-  HPS_INFO("Message rate: %lf", rate);
+  HPS_INFO("Message rate: time=%ld s and throughput=%lf", elapsed, rate);
 
   HPS_INFO("Done sending.. switching to receive");
   while (read < 4000) {
