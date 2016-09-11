77ad5e6860f33326074dbf71a4979e7d55b1258b
supunkamburugamuve <supun06@gmail.com>
2016-09-11 01:06:37 -0400

    cleaning up

--- test/heron_stmgr_server.h
+++ test/heron_stmgr_server.h
@@ -8,8 +8,6 @@
 #include <connection.h>
 #include <heron_rdma_server.h>
 #include "network_error.h"
-#include "proto/messages.h"
-#include "network.h"
 
 namespace heron {
   namespace common {
@@ -21,141 +19,86 @@ namespace heron {
   }
 }
 
-namespace heron {
-  namespace stmgr {
 
-    class StMgr;
+class StMgr;
 
-    class StMgrServer : public Server {
-    public:
-      StMgrServer(RDMAEventLoopNoneFD *eventLoop, const RDMAOptions &options, const sp_string &_topology_name,
-                  const sp_string &_topology_id, const sp_string &_stmgr_id,
-                  const std::vector<sp_string> &_expected_instances, StMgr *_stmgr,
-                  heron::common::MetricsMgrSt *_metrics_manager_client);
+class StMgrServer : public Server {
+public:
+StMgrServer(RDMAEventLoopNoneFD *eventLoop, const RDMAOptions &options, const sp_string &_topology_name,
+            const sp_string &_topology_id, const sp_string &_stmgr_id,
+            const std::vector<sp_string> &_expected_instances, StMgr *_stmgr,
+            heron::common::MetricsMgrSt *_metrics_manager_client);
 
-      virtual ~StMgrServer();
+virtual ~StMgrServer();
 
-      void SendToInstance(sp_int32 _task_id, const proto::stmgr::TupleMessage &_message);
+bool HaveAllInstancesConnectedToUs() const {
+  return active_instances_.size() == expected_instances_.size();
+}
 
-      void BroadcastNewPhysicalPlan(const proto::system::PhysicalPlan &_pplan);
+// Gets all the Instance information
+void GetInstanceInfo(std::vector<proto::system::Instance *> &_return);
 
-      // Do back pressure
-      void StartBackPressureClientCb(const sp_string &_other_stmgr_id);
+bool DidAnnounceBackPressure() { return !remote_ends_who_caused_back_pressure_.empty(); }
 
-      // Relieve back pressure
-      void StopBackPressureClientCb(const sp_string &_other_stmgr_id);
+protected:
+virtual void HandleNewConnection(Connection *newConnection);
 
-      bool HaveAllInstancesConnectedToUs() const {
-        return active_instances_.size() == expected_instances_.size();
-      }
+virtual void HandleConnectionClose(Connection *connection, NetworkErrorCode status);
 
-      // Gets all the Instance information
-      void GetInstanceInfo(std::vector<proto::system::Instance *> &_return);
+private:
+sp_string MakeBackPressureCompIdMetricName(const sp_string &instanceid);
 
-      bool DidAnnounceBackPressure() { return !remote_ends_who_caused_back_pressure_.empty(); }
+// Various handlers for different requests
 
-    protected:
-      virtual void HandleNewConnection(Connection *newConnection);
+// First from other stream managers
+void HandleStMgrHelloRequest(REQID _id, Connection *_conn,
+                             proto::stmgr::StrMgrHelloRequest *_request);
 
-      virtual void HandleConnectionClose(Connection *connection, NetworkErrorCode status);
+void HandleTupleStreamMessage(Connection *_conn, proto::stmgr::TupleStreamMessage *_message);
 
-    private:
-      sp_string MakeBackPressureCompIdMetricName(const sp_string &instanceid);
+// Backpressure message from and to other stream managers
+void SendStartBackPressureToOtherStMgrs();
 
-      sp_string GetInstanceName(Connection *_connection);
+void SendStopBackPressureToOtherStMgrs();
 
-      // Various handlers for different requests
+// Back pressure related connection callbacks
+// Do back pressure
+void StartBackPressureConnectionCb(Connection *_connection);
 
-      // First from other stream managers
-      void HandleStMgrHelloRequest(REQID _id, Connection *_conn,
-                                   proto::stmgr::StrMgrHelloRequest *_request);
+// Relieve back pressure
+void StopBackPressureConnectionCb(Connection *_connection);
 
-      void HandleTupleStreamMessage(Connection *_conn, proto::stmgr::TupleStreamMessage *_message);
 
-      // Next from local instances
-      void HandleRegisterInstanceRequest(REQID _id, Connection *_conn,
-                                         proto::stmgr::RegisterInstanceRequest *_request);
+// Compute the LocalSpouts from Physical Plan
+void ComputeLocalSpouts(const proto::system::PhysicalPlan &_pplan);
 
-      void HandleTupleSetMessage(Connection *_conn, proto::stmgr::TupleMessage *_message);
+// map from stmgr_id to their connection
+typedef std::map<sp_string, Connection *> StreamManagerConnectionMap;
+StreamManagerConnectionMap stmgrs_;
+// Same as above but reverse
+typedef std::map<Connection *, sp_string> ConnectionStreamManagerMap;
+ConnectionStreamManagerMap rstmgrs_;
 
-      // Backpressure message from and to other stream managers
-      void HandleStartBackPressureMessage(Connection *_conn,
-                                          proto::stmgr::StartBackPressureMessage *_message);
+// map from Connection to their task_id
+typedef std::map<Connection *, sp_int32> ConnectionTaskIdMap;
+ConnectionTaskIdMap active_instances_;
+// map of Instance_id/stmgrid to metric
+// Used for back pressure metrics
+typedef std::map<sp_string, heron::common::TimeSpentMetric *> InstanceMetricMap;
+InstanceMetricMap instance_metric_map_;
 
-      void HandleStopBackPressureMessage(Connection *_conn,
-                                         proto::stmgr::StopBackPressureMessage *_message);
+// instances/stream mgrs causing back pressure
+std::set<sp_string> remote_ends_who_caused_back_pressure_;
+// stream managers that have announced back pressure
+std::set<sp_string> stmgrs_who_announced_back_pressure_;
 
-      void SendStartBackPressureToOtherStMgrs();
+sp_string topology_name_;
+sp_string topology_id_;
+sp_string stmgr_id_;
+std::vector<sp_string> expected_instances_;
+StMgr *stmgr_;
 
-      void SendStopBackPressureToOtherStMgrs();
-
-      // Back pressure related connection callbacks
-      // Do back pressure
-      void StartBackPressureConnectionCb(Connection *_connection);
-
-      // Relieve back pressure
-      void StopBackPressureConnectionCb(Connection *_connection);
-
-      // Can we free the back pressure on the spouts?
-      void AttemptStopBackPressureFromSpouts();
-
-      // Start back pressure on the spouts
-      void StartBackPressureOnSpouts();
-
-      // Compute the LocalSpouts from Physical Plan
-      void ComputeLocalSpouts(const proto::system::PhysicalPlan &_pplan);
-
-      class InstanceData {
-      public:
-        explicit InstanceData(proto::system::Instance *_instance)
-            : instance_(_instance), local_spout_(false), conn_(NULL) {}
-
-        ~InstanceData() { delete instance_; }
-
-        void set_local_spout() { local_spout_ = true; }
-
-        void set_connection(Connection *_conn) { conn_ = _conn; }
-
-        proto::system::Instance *instance_;
-        bool local_spout_;
-        Connection *conn_;
-      };
-
-      // map from stmgr_id to their connection
-      typedef std::map<sp_string, Connection *> StreamManagerConnectionMap;
-      StreamManagerConnectionMap stmgrs_;
-      // Same as above but reverse
-      typedef std::map<Connection *, sp_string> ConnectionStreamManagerMap;
-      ConnectionStreamManagerMap rstmgrs_;
-
-      // map from Connection to their task_id
-      typedef std::map<Connection *, sp_int32> ConnectionTaskIdMap;
-      ConnectionTaskIdMap active_instances_;
-      // map of task id to InstanceData
-      // Once populated, will not change
-      typedef std::map<sp_int32, InstanceData *> TaskIdInstanceDataMap;
-      TaskIdInstanceDataMap instance_info_;
-
-      // map of Instance_id/stmgrid to metric
-      // Used for back pressure metrics
-      typedef std::map<sp_string, heron::common::TimeSpentMetric *> InstanceMetricMap;
-      InstanceMetricMap instance_metric_map_;
-
-      // instances/stream mgrs causing back pressure
-      std::set<sp_string> remote_ends_who_caused_back_pressure_;
-      // stream managers that have announced back pressure
-      std::set<sp_string> stmgrs_who_announced_back_pressure_;
-
-      sp_string topology_name_;
-      sp_string topology_id_;
-      sp_string stmgr_id_;
-      std::vector<sp_string> expected_instances_;
-      StMgr *stmgr_;
-
-      bool spouts_under_back_pressure_;
-    };
-
-  }  // namespace stmgr
-}  // namespace heron
+bool spouts_under_back_pressure_;
+};
 
 #endif  // SRC_CPP_SVCS_STMGR_SRC_MANAGER_STMGR_SERVER_H_
